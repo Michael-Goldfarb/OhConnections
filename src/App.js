35e6b6a57fc2b0073b618ea2026e90b19049506e
Michael-Goldfarb <michaelgoldfarb6@gmail.com>
2024-03-13 02:36:25 -0400

    Make background lighter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Connections03152024 from "./pages/inputs/03-15-2024/ConnectionsPage.js";
 import Connections03162024 from "./pages/inputs/03-16-2024/ConnectionsPage.js";
@@ -7,8 +7,18 @@ import Connections03182024 from "./pages/inputs/03-18-2024/ConnectionsPage.js";
 import Navbar from "./pages/components/NavBar.js";
 
 function App() {
+  useEffect(() => {
+    document.body.style.backgroundColor = "#2D3748";
+    document.body.style.color = "#FFF";
+
+    return () => {
+      document.body.style.backgroundColor = "";
+      document.body.style.color = "";
+    };
+  }, []);
+
   return (
-    <div className="app bg-gray-800 text-white">
+    <div className="app">
       <Router>
         <Navbar /> 
         <Routes>
