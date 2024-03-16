9d947a41a524781d40d6302500908cedcd3274ee
Michael-Goldfarb <michaelgoldfarb6@gmail.com>
2024-03-16 18:54:20 -0400

    Board 2

--- src/App.js
+++ src/App.js
@@ -27,8 +27,8 @@ function App() {
       <Router>
         <Navbar /> 
         <Routes>
-          <Route path="/" element={<Connections03152024 />} />
-          {/* <Route path="/03-15-2024" element={<Connections03152024 />} />  */}
+          <Route path="/" element={<Connections03162024 />} />
+          <Route path="/03-15-2024" element={<Connections03152024 />} /> 
          {/*  <Route path="/03-16-2024" element={<Connections03162024 />} />
           <Route path="/03-17-2024" element={<Connections03172024 />} />
           <Route path="/03-18-2024" element={<Connections03182024 />} />
