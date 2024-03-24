91e73fda115c83ef5370adc298d0efe64b71d483
Michael-Goldfarb <michaelgoldfarb6@gmail.com>
2024-03-24 19:16:58 -0400

    Board 11

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Connections03212024 from "./pages/inputs/03-21-2024/ConnectionsPage.js";
 import Connections03222024 from "./pages/inputs/03-22-2024/ConnectionsPage.js";
 import Connections03232024 from "./pages/inputs/03-23-2024/ConnectionsPage.js";
 import Connections03242024 from "./pages/inputs/03-24-2024/ConnectionsPage.js";
+import Connections03252024 from "./pages/inputs/03-25-2024/ConnectionsPage.js";
 import Navbar from "./pages/components/NavBar.js";
 
 function App() {
@@ -28,7 +29,7 @@ function App() {
       <Router>
         <Navbar /> 
         <Routes>
-          <Route path="/" element={<Connections03242024 />} />
+          <Route path="/" element={<Connections03252024 />} />
           <Route path="/03-15-2024" element={<Connections03152024 />} /> 
           <Route path="/03-16-2024" element={<Connections03162024 />} />
           <Route path="/03-17-2024" element={<Connections03172024 />} />
@@ -38,7 +39,7 @@ function App() {
           <Route path="/03-21-2024" element={<Connections03212024 />} />
           <Route path="/03-22-2024" element={<Connections03222024 />} /> 
           <Route path="/03-23-2024" element={<Connections03232024 />} />
-        {/*  <Route path="/03-24-2024" element={<Connections03242024 />} /> */}
+          <Route path="/03-24-2024" element={<Connections03242024 />} />
         </Routes>
       </Router>
     </div>
